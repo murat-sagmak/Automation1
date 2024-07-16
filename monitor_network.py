fefd714ce0a16962b2a863b72ba6e37ddda7beec
murat-sagmak <muratsagmak01@gmail.com>
2024-07-16 18:21:40 +0300

    network

--- monitor_network.py
+++ monitor_network.py
@@ -8,8 +8,6 @@ nr = InitNornir(config_file="config.yaml")
 def get_device_status(task):
     result = task.run(task=netmiko_send_command, command_string="show ip interface brief")
     task.host["status"] = result.result
+    print(result)
+    
 
-while True:
-    result = nr.run(task=get_device_status)
-    print_result(result)
-    time.sleep(5)  
